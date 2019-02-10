08963c7889a566900cf309dda97acde2cdd67177
Salif Mehmed <salif@programmer.net>
2019-02-10 20:29:15 +0200

    Tetris.jar

--- run/run.cmd
+++
@@ -1,8 +0,0 @@
-cd ../src
-%@Try%
-	java com.salifm.tetris.Main
-%@EndTry%
-:@Catch
-	javac com/salifm/tetris/*
-	java com.salifm.tetris.Main
-:@EndCatch
--- run/run.sh
+++
@@ -1,7 +0,0 @@
-cd ../src
-{
-	java com.salifm.tetris.Main
-} || {
-	javac com/salifm/tetris/*
-	java com.salifm.tetris.Main
-}
